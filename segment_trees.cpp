2ae62fd7d57b3fc4385fa69f4034a7d7ec4812c4
Dorl Neylon <65495639+dorlneylon@users.noreply.github.com>
2022-07-30 02:16:20 +0300

    updating the tree with O(logn) time complexity

--- segment_trees.cpp
+++ segment_trees.cpp
@@ -1,36 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void build(int v, int l, int r, vector<int> &st, vector<int> &A) {
+void build(int v, int l, int r, vector<int> &st, vector<int> &a) {
     if (r-l == 1) {
-        st[v] = A[l];
+        st[v] = a[l];
         return;
     }
     int m = (l+r)/2;
-    build(2*v+1, l, m, st, A);
-    build(2*v+2, m, r, st, A);
-    st[v] = max(st[2*v+1], st[2*v+2]);
+    build(2*v+1, l, m, st, a);
+    build(2*v+2, m, r, st, a);
+    st[v] = st[2*v+1] + st[2*v + 2];
 }
 
-int take_max(int v, int l, int r, vector<int> &st, int lx, int rx) {
-    if (lx <= l && rx >= r)
+int take(int v, int l, int r, int lx, int rx, vector<int> &st) {
+    if (lx >= r || rx <= l) {
+        return 0;
+    }
+    if (lx <= l && rx >= r) {
         return st[v];
-    if (lx >= r || rx <= l)
-        return -1;
+    }
     int m = (l+r)/2;
-    int lt = take_max(2*v+1, l, m, st, lx, rx);
-    int rt = take_max(2*v+2,m, r,st, lx, rx);
-    return max(lt, rt);
+    int lt = take(2*v+1, l, m, lx, rx, st);
+    int rt = take(2*v+2, m, r, lx, rx, st);
+    return lt+rt;
+}
+
+void update(int v, int l, int r, int x, int pos, vector<int> &st) {
+    if (r - l == 1) {
+        st[v] = x;
+        return;
+    }
+    int m = (l + r)/2;
+    if (pos < m) {
+        update(2*v+1, l, m, x, pos, st);
+    }
+    else {
+        update(2*v+2, m, r, x, pos, st);
+    }
+    st[v] = st[2*v+1] + st[2*v + 2];
 }
 
 int main() {
-    int n, l, r;
+    int n, l, r, ind, t;
     cin >> n;
-    cin >> l >> r;
+    vector <int> a(n);
     vector <int> st(4*n);
-    vector <int> A(n);
-    for (int i = 0; i < n; i++)
-        cin >> A[i];
-    build(0,0,n,st,A);
-    cout << take_max(0, 0, n, st, l-1, r);
+
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    build(0,0,n,st,a);
+
+    cin >> l >> r;
+    cout << take(0,0,n,l-1,r,st) << "\n";
+    cin >> ind >> t;
+
+    update(0,0,n,t,ind-1,st);
+
+    cout << take(0,0,n,l-1,r,st);
 }
