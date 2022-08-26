b4a2412fcadfe4f5f2a65ea345708a1a2a89d3ae
Dorl Neylon <65495639+dorlneylon@users.noreply.github.com>
2022-08-27 01:58:34 +0300

    jesus we made it

--- effective segment trees.cpp
+++ effective segment trees.cpp
@@ -2,55 +2,49 @@
 using namespace std;
 
 const int N = 1e5; // array size limit
-int n, h, q, tt, l, r;
+int n, h, q, tt, lx, rx;
 int t[2*N], d[N];
 
-void calc(int p) {
-    if (d[p] == 0) t[p] = t[p<<1] + t[p<<1|1];
-    else t[p] += d[p];
+void apply(int p, int val, int k) {
+    t[p] += val * k;
+    if (p < n) d[p] += val;
 }
 
-void apply(int p, int value) {
-    t[p] += value;
-    if (p < n) d[p] += value;
-}
-
-void build(int l, int r) {
-    for (l += n, r += n-1; l > 1;) {
-        l >>= 1, r >>= 1;
-        for (int i = r; i >= l; --i) calc(i);
+void push(int p) {
+    for (int s = h; s > 0; --s) {
+        int i = p >> s, k = 1 << (s - 1);
+        if (d[i] != 0) {
+            apply(i<<1, d[i], k);
+            apply(i<<1|1, d[i], k);
+            d[i] = 0;
+        }
     }
 }
 
-void push(int l, int r) {
-    int s = h;
-    for (l += n, r += n-1; s > 0; --s)
-        for (int i = l >> s; i <= r >> s; ++i)
-            if (d[i] != 0) {
-                apply(i<<1, d[i]);
-                apply(i<<1|1, d[i]);
-                d[i] = 0;
-            }
+void build() {  // build the tree
+    for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
 }
 
-void modify(int l, int r, int value) {
-    if (value == 0) return;
-    push(l, l + 1);
-    push(r - 1, r);
-    int l0 = l, r0 = r, k = 1;
-    for (l += n, r += n; l < r; l >>= 1, r >>= 1, k <<= 1) {
-        if (l&1) apply(l++, value);
-        if (r&1) apply(--r, value);
+void build(int p) {
+    for (int k = 2; p > 1; k<<=1) p>>=1, t[p] = t[p<<1] + t[p<<1|1] + d[p]*k;
+}
+
+void incr(int l, int r, int val) {
+    l += n, r += n;
+    int l0 = l, r0 = r;
+    for (int k = 1; l < r; l >>= 1, r >>= 1, k<<=1) {
+        if (l&1) apply(l++, val, k);
+        if (r&1) apply(--r, val, k);
     }
-    build(l0, l0 + 1);
-    build(r0 - 1, r0);
+    build(l0);
+    build(r0-1);
 }
 
 int query(int l, int r) {
-    push(l, l + 1);
-    push(r - 1, r);
+    l += n, r += n;
     int res = 0;
-    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+    push(l), push(r-1);
+    for (; l < r; l >>= 1, r >>= 1) {
         if (l&1) res += t[l++];
         if (r&1) res += t[--r];
     }
@@ -62,15 +56,15 @@ int main() {
     h = __lg(n);
     for (int i = 0; i < n; ++i) cin >> t[n + i];
     cin >> q;
+    build();
+
     while (q --> 0) {
-        cin >> tt >> l >> r;
+        cin >> tt >> lx >> rx;
         if (tt == 1) {
             int val;
             cin >> val;
-            modify(l, r, val);
-            push(l, r);
+            incr(lx, rx+1, val);
         }
-        build(l, r);
-        cout << query(l, r) << "\n";
+        cout << query(lx, rx+1) << "\n";
     }
 }
