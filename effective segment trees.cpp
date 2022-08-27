97cccb594482968d501057954fbac263c6d29b93
Dorl Neylon <65495639+dorlneylon@users.noreply.github.com>
2022-08-27 21:50:28 +0300

    change modification

--- effective segment trees.cpp
+++ effective segment trees.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5; // array size limit
+const int N = 1e5;
 int n, h, q, tt, lx, rx;
 int t[2*N], d[N];
 
 void apply(int p, int val, int k) {
-    t[p] += val * k;
+    t[p] += val*k;
     if (p < n) d[p] += val;
 }
 
@@ -21,50 +21,56 @@ void push(int p) {
     }
 }
 
-void build() {  // build the tree
-    for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+void build() {
+    for (int p = n - 1; p > 0; --p) t[p] = t[p<<1] + t[p<<1|1];
 }
 
 void build(int p) {
-    for (int k = 2; p > 1; k<<=1) p>>=1, t[p] = t[p<<1] + t[p<<1|1] + d[p]*k;
+    for (int k = 2; p > 1; k<<=1, p>>=1) t[p>>1] = t[p] + t[p^1] + d[p>>1]*k;
 }
 
-void incr(int l, int r, int val) {
+void modify(int l, int r, int val) {
     l += n, r += n;
     int l0 = l, r0 = r;
-    for (int k = 1; l < r; l >>= 1, r >>= 1, k<<=1) {
+    for (int k = 1; l < r; l>>=1, r>>=1, k<<=1) {
         if (l&1) apply(l++, val, k);
         if (r&1) apply(--r, val, k);
     }
-    build(l0);
-    build(r0-1);
+    build(l0), build(r0-1);
 }
 
 int query(int l, int r) {
     l += n, r += n;
     int res = 0;
     push(l), push(r-1);
-    for (; l < r; l >>= 1, r >>= 1) {
+    for (; l < r; l>>=1, r>>=1) {
         if (l&1) res += t[l++];
         if (r&1) res += t[--r];
     }
     return res;
 }
 
+void change(int p, int val) {
+    for (t[p+=n] = val; p > 1; p>>=1) t[p>>1] = t[p] + t[p^1];
+}
+
 int main() {
     cin >> n;
     h = __lg(n);
     for (int i = 0; i < n; ++i) cin >> t[n + i];
-    cin >> q;
     build();
+    cin >> q;
 
     while (q --> 0) {
         cin >> tt >> lx >> rx;
         if (tt == 1) {
-            int val;
-            cin >> val;
-            incr(lx, rx+1, val);
+            int temp;
+            cin >> temp;
+            modify(lx, rx+1, temp);
+        } else if (tt == 2) {
+            change(lx, rx);
+        } else {
+            cout << query(lx, rx+1) << "\n";
         }
-        cout << query(lx, rx+1) << "\n";
     }
 }
